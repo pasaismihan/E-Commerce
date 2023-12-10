cc96d8747ec2ffbf30a807aaab8a09c039d42dab
pasaismihan <pasa_13142@hotmail.com>
2023-12-10 18:09:28 +0300

    guncel

--- e-commerce/app/components/home/Banner.tsx
+++ e-commerce/app/components/home/Banner.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image"
+import Image from "@/node_modules/next/image";
 
 const Banner = () => {
-    return (
-        <div className="h-[237px] bg-black flex items-center justify-center">
-            <div className="h-[137px] relative w-full">
-                <Image src={"/kampanya.jpg"} fill alt="" className="object-cover"/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="h-[237px] bg-black flex items-center justify-center">
+      <div className="h-[137px] relative w-full">
+        <Image src={"/kampanya.jpg"} fill alt="" className="object-cover" />
+      </div>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
--- e-commerce/app/components/navbar/Logo.tsx
+++ e-commerce/app/components/navbar/Logo.tsx
@@ -1,9 +1,17 @@
-"use client"
+"use client";
+
+import { useRouter } from "next/navigation";
 
 const Logo = () => {
-    return (
-        <div className="bg-orange-700 px-2 py-1 rounded-md text-lg md:text-2xl cursor-pointer">Burada <span className="text-sm">.com</span></div>
-    )
-}
+  const router = useRouter();
+  return (
+    <div
+      onClick={() => router.push("/")}
+      className="bg-orange-700 px-2 py-1 rounded-md text-lg md:text-2xl cursor-pointer"
+    >
+      Burada <span className="text-sm">.com</span>
+    </div>
+  );
+};
 
-export default Logo 
+export default Logo;
