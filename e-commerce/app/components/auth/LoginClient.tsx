add7d04fcda265accd49e5ba8fe53f42a2b2dd59
Paşa İSMİHAN <119595276+pasaismihan@users.noreply.github.com>
2023-12-22 13:46:42 +0300

    authentication icin login ve register olusturuldu

---
+++ e-commerce/app/components/auth/LoginClient.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AuthContainer from '../containers/AuthContainer'
+
+const LoginClient = () => {
+    return (
+        <AuthContainer>
+            login
+        </AuthContainer>
+    )
+}
+
+export default LoginClient
---
+++ e-commerce/app/components/auth/RegisterClient.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import AuthContainer from "../containers/AuthContainer"
+
+const RegisterClient = () => {
+  return (
+    <AuthContainer>
+        Register
+    </AuthContainer>
+  )
+}
+
+export default RegisterClient
