2a057ab5cd8a9f55fc79fc06905138202e00fdb8
pasaismihan <pasa_13142@hotmail.com>
2023-12-30 16:01:58 +0300

    nextauth ve bycrpt kullanilarak password hashed islemi yapildi

---
+++ e-commerce/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,57 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/libs/prismadb";
+import bycrpt from "bcrypt";
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("Gecersiz email veya parola...");
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user.hashedPassword) {
+          throw new Error("gecersiz email veya sifre");
+        }
+
+        const comparePassword = await bycrpt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        if (!comparePassword) {
+          throw new Error("yanlis sifre");
+        }
+
+        return user;
+      },
+    }),
+  ],
+
+  pages: {
+    signIn: "/login",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+});
