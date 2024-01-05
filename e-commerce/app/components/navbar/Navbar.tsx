aa69731f2a181ba9591d057d7a5ead2274da4236
Paşa İSMİHAN <119595276+pasaismihan@users.noreply.github.com>
2024-01-05 18:14:07 +0300

    navbar uzerindeki user girisi duzenlendi

--- e-commerce/app/components/navbar/Navbar.tsx
+++ e-commerce/app/components/navbar/Navbar.tsx
@@ -1,16 +1,18 @@
+import { getCurrentUser } from "@/app/actions/getCurrentUser"
 import CardCount from "./CardCount"
 import HamburgerMenu from "./HamburgerMenu"
 import Logo from "./Logo"
 import Search from "./Search"
 import User from "./User"
 
-const Navbar = () => {
+const Navbar = async () => {
+  const currentUser = await getCurrentUser();
   return (
     <div className="flex items-center justify-between gap-3 md:gap-10 px-3 h-16 bg-orange-600 text-slate-100">
       <Logo />
       <Search />
       <CardCount />
-      <User />
+      <User currentUser = {currentUser}/>
       <HamburgerMenu />
     </div>
   )
